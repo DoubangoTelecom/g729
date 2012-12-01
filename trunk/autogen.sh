ec0259002b83ce85784471c0caaf50b8ee571cab
bossiel@yahoo.fr <bossiel@yahoo.fr@edcfe46c-7725-dae6-294e-112f47efd222>
2012-12-01 00:27:31 +0000

    Add support for GNU autotools

---
+++ trunk/autogen.sh
@@ -0,0 +1,14 @@
+echo "aclocal"
+aclocal
+
+echo "autoheader"
+autoheader
+
+echo "create NEWS, README, AUTHORS and ChangeLog"
+touch NEWS README AUTHORS ChangeLog
+
+echo "autoreconf"
+autoreconf -i
+
+echo "automake"
+automake -a
